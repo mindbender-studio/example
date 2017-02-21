8c15f8e05fead4370a81a741036e672cfa6c5434
Marcus Ottosson <konstruktion@gmail.com>
2017-02-21 18:08:26 +0000

    Add docs and project variables

--- projects/hulk/assets/Bruce.bat
+++ projects/hulk/assets/Bruce.bat
@@ -1,2 +1,7 @@
+:: Following is the commands that needs to be called in the bat
+::
+:: call _mkasset
+:: %~n0 is the name of this project as the name of the .bat
+:: %~dp0 is drive plus path variable
 @echo off
 call _mkasset %~n0 %~dp0%
--- projects/spiderman.bat
+++ projects/spiderman.bat
@@ -1,2 +1,21 @@
+:: Following is the commands that needs to be called in the bat
+::
+:: call _mkproject
+:: %~dp0 is drive plus path variable
+:: %~n0 is the name of this project as the name of the .bat
+:: %1 is the entry by the user for asset
 @echo off
-call _mkproject %~dp0 %~n0 %1
+call _mkproject %~dp0 %~n %1
+
+:: --------------------
+:: Additional Variables
+:: --------------------
+
+:: (int): Current frames per second for the project
+set MINDBENDER_FPS=30
+
+:: (str): Render resolution width
+set MINDBENDER_RESOLUTION_WIDTH=1280
+
+:: (str): Render resolution height
+set MINDBENDER_RESOLUTION_HEIGHT=720
