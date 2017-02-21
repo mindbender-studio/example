734d98f086e72d530a4a1db9102ca39262b444b8
Marcus Ottosson <konstruktion@gmail.com>
2017-02-21 18:09:23 +0000

    Update to core 2.0

--- projects/spiderman.bat
+++ projects/spiderman.bat
@@ -5,7 +5,7 @@
 :: %~n0 is the name of this project as the name of the .bat
 :: %1 is the entry by the user for asset
 @echo off
-call _mkproject %~dp0 %~n %1
+call _mkproject %~dp0 %~n0 %~n0 %1
 
 :: --------------------
 :: Additional Variables
